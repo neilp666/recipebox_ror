bcd37e1a023daa4eeef9b7f610ba64d023ac0889
neil patel <neil@myindianweddingapp.com>
2016-07-17 22:04:47 +0100

    Added controller & views

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,6 +2,7 @@ class RecipesController < ApplicationController
   before_action :find_recipe, only: [:show, :edit, :update, :destroy]
 
   def index
+    @recipe = Recipe.all.order("created_at DESC")
   end
 
   def new
@@ -11,8 +12,33 @@ class RecipesController < ApplicationController
   def show
   end
 
-  def create
+  def edit
   end
+
+   def create
+    @recipe = Recipe.new(recipe_params)
+
+    if @recipe.save
+      redirect_to @recipe, notice: "Successfully created new recipe"
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    if @recipe.update(recipe_params)
+      redirect_to @recipe
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @recipe.destroy
+    redirect_to root_path, notice: "Successfully deleted recipe"
+  end
+
+ 
 
   private
 
